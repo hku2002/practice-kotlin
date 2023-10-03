4494c960028f892c4b9a47270d7e61e0d5a23d4f
Hwang <hku2002@hanmail.net>
2023-10-03 14:26:41 +0900

    feat: 변수에 null 넣는법 추가

--- kotlin-basic/src/main/kotlin/com/practice/ex1/ex1Main.kt
+++ kotlin-basic/src/main/kotlin/com/practice/ex1/ex1Main.kt
@@ -13,6 +13,14 @@ fun main() {
     val number2 = 20L
     println(number2)
 
+    // null 을 변수에 넣으려면 타입과 ?을 사용해야 한다.
+    var number3: Long? = null
+    println(number3)
+    number3 = 30L;
+    println(number3)
+    val sampleNumber: Long? = null
+    println(sampleNumber)
+
 
 
 }
