6f86615c72e397f1742bc1043a55ee7071b0c446
Hwang <hku2002@hanmail.net>
2023-10-03 21:32:11 +0900

    feat: 스마트 캐스트 연습

--- kotlin-basic/src/main/kotlin/com/practice/ex3/ex3Main.kt
+++ kotlin-basic/src/main/kotlin/com/practice/ex3/ex3Main.kt
@@ -1,8 +1,16 @@
 package com.practice.ex3
 
+import com.practice.ex2.Person
+
 fun main() {
     val number1 = 3
     //val number2: Long = number1  <-- 암시적으로 변환 불가
     val number2: Long = number1.toLong();
     println(number1 + number2)
+}
+
+fun printAgeIfPerson(obj: Any) {
+    if (obj is Person) {
+        println(obj.name)
+    }
 }
