47374ada53a5f69ce5fee58b2428a5fec1b8ba1d
Hwang <hku2002@hanmail.net>
2023-10-03 14:21:55 +0900

    feat: 코틀린 기초 연습 최초 추가

---
+++ kotlin-basic/src/main/kotlin/com/practice/ex1/ex1Main.kt
@@ -0,0 +1,14 @@
+package com.practice.ex1
+
+fun main() {
+
+    var number1 = 10L
+    println(number1)
+
+    number1 = 20L
+    println(number1)
+
+    val number2 = 20L
+    println(number2)
+
+}
