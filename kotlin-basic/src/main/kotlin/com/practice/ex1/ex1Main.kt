bec614b8dc0174b7c0b372b52826387e4050f787
Hwang <hku2002@hanmail.net>
2023-10-03 14:24:00 +0900

    style: 주석 추가

--- kotlin-basic/src/main/kotlin/com/practice/ex1/ex1Main.kt
+++ kotlin-basic/src/main/kotlin/com/practice/ex1/ex1Main.kt
@@ -2,13 +2,17 @@ package com.practice.ex1
 
 fun main() {
 
+    // var 은 가변이다.
     var number1 = 10L
     println(number1)
 
     number1 = 20L
     println(number1)
 
+    // val 은 불변이다.
     val number2 = 20L
     println(number2)
+
+
 
 }
