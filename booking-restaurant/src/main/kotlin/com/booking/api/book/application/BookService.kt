cb4e6c4845361c072572d6669589ff16f895b22d
hku2002 <hku2002@hanmail.net>
2024-05-04 20:58:18 +0900

    feat: 예약 조회 메소드 생성

--- booking-restaurant/src/main/kotlin/com/booking/api/book/application/BookService.kt
+++ booking-restaurant/src/main/kotlin/com/booking/api/book/application/BookService.kt
@@ -1,8 +1,17 @@
 package com.booking.api.book.application
 
+import com.booking.api.book.domain.repository.BookRepository
+import com.booking.api.book.domain.repository.RestaurantBookTimeRepository
 import org.springframework.stereotype.Service
 
 @Service
 class BookService(
+        private val bookRepository: BookRepository,
+        private val restaurantBookTimeRepository: RestaurantBookTimeRepository
 ) {
+
+    fun findBooksFromRestaurant(restaurantId: Long) {
+        restaurantBookTimeRepository.findAllByRestaurantId(restaurantId)
+    }
+
 }
--- booking-restaurant/src/main/kotlin/com/booking/api/book/domain/repository/RestaurantBookTimeRepository.kt
+++ booking-restaurant/src/main/kotlin/com/booking/api/book/domain/repository/RestaurantBookTimeRepository.kt
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository
 
 @Repository
 interface RestaurantBookTimeRepository: JpaRepository<RestaurantBookTime, Long> {
+    fun findAllByRestaurantId(id: Long): List<RestaurantBookTime>
 }
