a57ac3e97a3950ccc288327f9460bf31e61caa24
hku2002 <hku2002@hanmail.net>
2024-05-05 17:34:25 +0900

    feat: 트렌젝션 추가

--- booking-restaurant/src/main/kotlin/com/booking/api/book/application/BookService.kt
+++ booking-restaurant/src/main/kotlin/com/booking/api/book/application/BookService.kt
@@ -4,8 +4,10 @@ import com.booking.api.book.domain.repository.BookRepository
 import com.booking.api.book.domain.repository.RestaurantBookTimeRepository
 import com.booking.api.book.dto.RestaurantBooksResponse
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
+@Transactional(readOnly = true)
 class BookService(
         private val bookRepository: BookRepository,
         private val restaurantBookTimeRepository: RestaurantBookTimeRepository
