c986eccc44802d6abadc0596bded725fc1aeb705
hku2002 <hku2002@hanmail.net>
2024-05-05 17:16:13 +0900

    feat: entity list -> dto list 변환 로직 추가

--- booking-restaurant/src/main/kotlin/com/booking/api/book/application/BookService.kt
+++ booking-restaurant/src/main/kotlin/com/booking/api/book/application/BookService.kt
@@ -2,6 +2,7 @@ package com.booking.api.book.application
 
 import com.booking.api.book.domain.repository.BookRepository
 import com.booking.api.book.domain.repository.RestaurantBookTimeRepository
+import com.booking.api.book.dto.RestaurantBooksResponse
 import org.springframework.stereotype.Service
 
 @Service
@@ -10,8 +11,9 @@ class BookService(
         private val restaurantBookTimeRepository: RestaurantBookTimeRepository
 ) {
 
-    fun findBooksFromRestaurant(restaurantId: Long) {
-        restaurantBookTimeRepository.findAllByRestaurantId(restaurantId)
+    fun findBooksFromRestaurant(restaurantId: Long): List<RestaurantBooksResponse> {
+        val restaurantBookTimes = restaurantBookTimeRepository.findAllByRestaurantId(restaurantId)
+        return RestaurantBooksResponse.of(restaurantBookTimes)
     }
 
 }
--- booking-restaurant/src/main/kotlin/com/booking/api/book/dto/RestaurantBooksResponse.kt
+++ booking-restaurant/src/main/kotlin/com/booking/api/book/dto/RestaurantBooksResponse.kt
@@ -1,8 +1,24 @@
 package com.booking.api.book.dto
 
+import com.booking.api.book.domain.entity.RestaurantBookTime
 import java.time.LocalDateTime
 
 class RestaurantBooksResponse(
         val bookTime: LocalDateTime,
         val isBooked: Boolean = false
-)
+) {
+    companion object {
+        fun of(restaurantBookTime: RestaurantBookTime): RestaurantBooksResponse {
+            return RestaurantBooksResponse(
+                    bookTime = restaurantBookTime.bookTime,
+                    isBooked = restaurantBookTime.isBooked
+            )
+        }
+
+        fun of(restaurantBookTimes: List<RestaurantBookTime>): List<RestaurantBooksResponse> {
+            return restaurantBookTimes.stream()
+                    .map { restaurantBookTime: RestaurantBookTime -> of(restaurantBookTime) }
+                    .toList();
+        }
+    }
+}
