00b13d4289fed14478043aa8ac30940f1cfd3ab3
hku2002 <hku2002@hanmail.net>
2024-05-05 17:41:02 +0900

    feat: 예약 요청 처리 메소드 추가

--- booking-restaurant/src/main/kotlin/com/booking/api/book/application/BookService.kt
+++ booking-restaurant/src/main/kotlin/com/booking/api/book/application/BookService.kt
@@ -2,6 +2,7 @@ package com.booking.api.book.application
 
 import com.booking.api.book.domain.repository.BookRepository
 import com.booking.api.book.domain.repository.RestaurantBookTimeRepository
+import com.booking.api.book.dto.BookRequest
 import com.booking.api.book.dto.RestaurantBooksResponse
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -16,6 +17,9 @@ class BookService(
     fun findBooksFromRestaurant(restaurantId: Long): List<RestaurantBooksResponse> {
         val restaurantBookTimes = restaurantBookTimeRepository.findAllByRestaurantId(restaurantId)
         return RestaurantBooksResponse.of(restaurantBookTimes)
+    }
+
+    fun addBookByRestaurantId(request: BookRequest) {
     }
 
 }
