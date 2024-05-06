511b042b373508706b32b7f748f14972d9ec5aec
hku2002 <hku2002@hanmail.net>
2024-05-06 20:47:42 +0900

    feat: email, password 추가

--- booking-restaurant/src/main/kotlin/com/booking/api/user/domain/entity/User.kt
+++ booking-restaurant/src/main/kotlin/com/booking/api/user/domain/entity/User.kt
@@ -5,12 +5,15 @@ import jakarta.persistence.*
 
 @Entity
 @Table(uniqueConstraints = [
-    UniqueConstraint(name = "unq-phone_number", columnNames = ["phone_number"])
+    UniqueConstraint(name = "unq-phone_number", columnNames = ["phone_number"]),
+    UniqueConstraint(name = "unq-email", columnNames = ["email"])
 ])
 class User(
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         val id: Long,
+        val email: String,
+        val password: String,
         val userName: String,
         val phoneNumber: String,
 ): BaseEntity()
