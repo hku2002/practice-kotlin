af10631b89d07c388060c4c006d15c9e697b1eee
hku2002 <hku2002@hanmail.net>
2024-05-06 21:09:09 +0900

    feat: swagger 설명 추가

--- booking-restaurant/src/main/kotlin/com/booking/api/book/api/BookController.kt
+++ booking-restaurant/src/main/kotlin/com/booking/api/book/api/BookController.kt
@@ -3,6 +3,13 @@ package com.booking.api.book.api
 import com.booking.api.book.application.BookService
 import com.booking.api.book.dto.RestaurantBooksResponse
 import com.booking.api.common.response.BaseResponse
+import com.booking.api.restaurant.dto.RestaurantResponse
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.ArraySchema
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -14,6 +21,12 @@ class BookController(
         private val bookService: BookService
 ) {
 
+    @Operation(summary = "레스토랑 예약 목록 조회", description = "레스토랑 id로 예약 목록을 조회하는 API")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "Success", content = [
+            Content(mediaType = "application/json", array = ArraySchema(schema = Schema(implementation = RestaurantResponse::class)))
+        ])
+    ])
     @GetMapping("/restaurants/{id}")
     fun findBooksFromRestaurant(@PathVariable id: Long): BaseResponse<List<RestaurantBooksResponse>> {
         return BaseResponse(bookService.findBooksFromRestaurant(restaurantId = id))
--- booking-restaurant/src/main/kotlin/com/booking/api/restaurant/api/RestaurantController.kt
+++ booking-restaurant/src/main/kotlin/com/booking/api/restaurant/api/RestaurantController.kt
@@ -3,6 +3,12 @@ package com.booking.api.restaurant.api
 import com.booking.api.common.response.BaseResponse
 import com.booking.api.restaurant.application.RestaurantService
 import com.booking.api.restaurant.dto.RestaurantResponse
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.ArraySchema
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -13,6 +19,12 @@ class RestaurantController(
         private val restaurantService: RestaurantService
 ) {
 
+    @Operation(summary = "레스토랑 목록 조회", description = "레스토랑 전체 목록을 조회하는 API")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "Success", content = [
+            Content(mediaType = "application/json", array = ArraySchema(schema = Schema(implementation = RestaurantResponse::class)))
+        ])
+    ])
     @GetMapping
     fun findRestaurants(): BaseResponse<List<RestaurantResponse>> {
         return BaseResponse(restaurantService.findRestaurants())
