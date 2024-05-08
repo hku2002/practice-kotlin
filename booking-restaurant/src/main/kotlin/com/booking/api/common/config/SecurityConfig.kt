d9c377cbd6bd82d53fe805974c0cc3028e5e65fd
hku2002 <hku2002@hanmail.net>
2024-05-08 23:08:38 +0900

    faet: filterChain 설정 추가

--- booking-restaurant/src/main/kotlin/com/booking/api/common/config/SecurityConfig.kt
+++ booking-restaurant/src/main/kotlin/com/booking/api/common/config/SecurityConfig.kt
@@ -3,6 +3,7 @@ package com.booking.api.common.config
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
 
 
@@ -11,6 +12,16 @@ class SecurityConfig {
 
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
-        return http.build();
+        http
+            .csrf { it.disable() }
+            .headers { it.frameOptions { frameOptions -> frameOptions.sameOrigin() } }
+            .authorizeHttpRequests {
+                it.requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                    .anyRequest().authenticated()	// 그 외의 모든 요청은 인증 필요
+            }
+            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+            //.apply(JwtSecurityConfig(jwtService))
+        return http.build()
     }
+
 }
