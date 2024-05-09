2c4dc25e6a32edb1d6b27b635cf708d82102d497
hku2002 <hku2002@hanmail.net>
2024-05-09 22:02:30 +0900

    feat: /user 접근 제한 해제

--- booking-restaurant/src/main/kotlin/com/booking/api/common/config/SecurityConfig.kt
+++ booking-restaurant/src/main/kotlin/com/booking/api/common/config/SecurityConfig.kt
@@ -16,7 +16,7 @@ class SecurityConfig {
             .csrf { it.disable() }
             .headers { it.frameOptions { frameOptions -> frameOptions.sameOrigin() } }
             .authorizeHttpRequests {
-                it.requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                it.requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/user/**").permitAll()
                     .anyRequest().authenticated()	// 그 외의 모든 요청은 인증 필요
             }
             .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
