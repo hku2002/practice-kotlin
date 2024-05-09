d54d1467ad52a47e2b288f12d3f1f29fc8ef3254
hku2002 <hku2002@hanmail.net>
2024-05-09 22:01:24 +0900

    feat: swagger 설명 추가

--- booking-restaurant/src/main/kotlin/com/booking/api/user/api/UserController.kt
+++ booking-restaurant/src/main/kotlin/com/booking/api/user/api/UserController.kt
@@ -3,6 +3,12 @@ package com.booking.api.user.api
 import com.booking.api.common.response.BaseResponse
 import com.booking.api.user.application.UserService
 import com.booking.api.user.dto.UserJoinRequest
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.ArraySchema
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -14,6 +20,12 @@ class UserController(
     private val userService: UserService
 ) {
 
+    @Operation(summary = "회원 가입", description = "회원 가입 API")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "Success", content = [
+            Content(mediaType = "application/json", array = ArraySchema(schema = Schema(implementation = String::class)))
+        ])
+    ])
     @PostMapping("/join")
     fun findRestaurants(userJoinRequest: UserJoinRequest): BaseResponse<String> {
         return BaseResponse(userService.join(userJoinRequest))
