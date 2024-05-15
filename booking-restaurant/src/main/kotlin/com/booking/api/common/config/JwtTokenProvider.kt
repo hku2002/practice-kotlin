ad0ffc674ccf0378a78f94561c447ad6269386d3
hku2002 <hku2002@hanmail.net>
2024-05-15 18:05:51 +0900

    faet: createToken 로직 추가

--- booking-restaurant/src/main/kotlin/com/booking/api/common/config/JwtTokenProvider.kt
+++ booking-restaurant/src/main/kotlin/com/booking/api/common/config/JwtTokenProvider.kt
@@ -1,9 +1,15 @@
 package com.booking.api.common.config
 
 import com.booking.api.common.dto.JwtToken
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
+import io.jsonwebtoken.io.Decoders
+import io.jsonwebtoken.security.Keys
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.core.Authentication
+import org.springframework.security.core.GrantedAuthority
+import java.util.*
 
 const val EXPIRE_TIME_MILLISECONDS: Long = 1000 * 60 * 30
 
@@ -13,9 +19,25 @@ class JwtTokenProvider {
     @Value("jwt.secret")
     lateinit var secretKey: String
 
+    private val key by lazy { Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey)) }
+
 
     fun createToken(authentication: Authentication): JwtToken {
-        return JwtToken("", "")
+        val authorities: String = authentication
+            .authorities
+            .joinToString(",", transform = GrantedAuthority::getAuthority)
+
+        val now = Date()
+        val accessExpiration = Date(now.time+ EXPIRE_TIME_MILLISECONDS)
+        val accessToken = Jwts.builder()
+            .subject(authentication.name)
+            .claim("auth", authorities)
+            .issuedAt(now)
+            .expiration(accessExpiration)
+            .signWith(key, SignatureAlgorithm.HS512)
+            .compact()
+
+        return JwtToken("Bearer", accessToken)
     }
 
 }
